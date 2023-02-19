cab42d47bd2cf36a798faeba0de9f0f2c893b787
EgorPayrkov <120778556+EgorPayrkov@users.noreply.github.com>
2023-02-19 12:50:18 +0300

    Update games_tags.py

--- games/templatetags/games_tags.py
+++ games/templatetags/games_tags.py
@@ -18,6 +18,6 @@ def show_categories(sort=None, cat_selected=0):
     if not sort:
         cats = Category.objects.all()
     else:
-        cats = cats = Category.objects.order_by(sort)
+        cats = Category.objects.order_by(sort)
 
     return {'cats':cats, 'cat_selected':cat_selected}
